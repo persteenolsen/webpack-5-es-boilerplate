8b199921b784a04b2ab9384810efee2414dc0aed
Per Steen Olsen <persteen22@hotmail.com>
2021-05-11 14:14:38 +0200

    Adjusting the layout of the About page to be more readable

--- src/views/pages/About.js
+++ src/views/pages/About.js
@@ -7,54 +7,54 @@ let About = {
 			            		  
                 <h2>Description</h2><br />
   
-                This single page application was made in plain JavaScript with a Webpack 5 configuration
+                - This single page application was made in plain JavaScript with a Webpack 5 configuration
                 <br/> <br/>
 				
                 <b>Last Updated</b>  <br />
-                29-04-2021 <br /> <br />
+                - 11-05-2021 <br /> <br />
                 
 				
                 <b>Main functionality</b> <br />
                 				                 
-			    Showing the power of Webpack <br />
-				Hot Reload / HMR by Webpack Dev Server<br />
-				Adding partly support for some older Browsers like IE 11<br />
-                Showing routing without any framework or library <br />
-				Handling 404 routing errors by a custom error page <br />
-                Serving static assets like images, styles and fonts <br />
-			    GET, POST, PUT and DELETE operations towards the test Web API <br /> <br />
+			    - Showing the power of Webpack <br />
+				- Hot Reload / HMR by Webpack Dev Server<br />
+				- Adding support for some older Browsers like IE 11<br />
+                - Showing routing without any framework or library <br />
+				- Handling 404 routing errors by a custom error page <br />
+                - Serving static assets like images, styles and fonts <br />
+			    - GET, POST, PUT and DELETE operations towards the test Web API <br /> <br />
                
 			   
                 <b>Module Bundler</b> <br />
                 
-                Manuel configuration of the Webpack 5 from scratch <br /> <br />
+                - Manuel configuration of the Webpack 5 from scratch <br /> <br />
 				
                 
 				<b>The single page application - The frontend</b> <br />
             
-                PostCSS <br />
-                HTML <br />
-                Mainly JavaScript ES5, ES6 and ES7 <br />
-                Bootstrap 4 by CDN for the responsive design <br /> <br />
+                - PostCSS <br />
+                - HTML <br />
+                - Mainly JavaScript ES5, ES6 and ES7 <br />
+                - Bootstrap 4 by CDN for the responsive design <br /> <br />
 
             
                 <b>The Web API - The backend</b> <br />
  
-                The Web API is jsonplaceholder used for testing frontends <br /> <br />
+                - The Web API is jsonplaceholder used for testing frontends <br /> <br />
 
                 				
                 <b>Security</b> <br />
              
-                The only security is the use of HTTPS because it is a demo only <br /> <br />
+                - The only security is the use of HTTPS because it is a demo only <br /> <br />
 				
 				
 				<b>Hosting</b> <br />
-                The application is hosted at a traditionel Webhotel <br /> <br />
+                - The application is hosted at a traditionel Webhotel <br /> <br />
              
 
                 <b>Text Editor</b> <br />
              
-			    Visual Studio Code <br />
+			    - Visual Studio Code <br />
             			 
 				
             </section>
